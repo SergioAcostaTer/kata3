ac05a80285eba564f492129e7b9f7f0e1aa43a89
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2024-12-08 14:22:13 +0100

    kata 2 maven project

---
+++ src/main/java/software/ulpgc/kata3/TitleDeserializer.java
@@ -0,0 +1,6 @@
+package software.ulpgc.kata3;
+
+
+public interface TitleDeserializer {
+    Title deserialize(String line);
+}
---
+++ src/main/java/software/ulpgc/kata3/TitleLoader.java
@@ -0,0 +1,9 @@
+package software.ulpgc.kata3;
+
+
+import java.io.IOException;
+import java.util.List;
+
+public interface TitleLoader {
+    List<Title> load() throws IOException;
+}
